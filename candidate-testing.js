aee7459173dff6697f77b7a28e74ef762fdabcd8
woodc555 <wood.c9595@gmail.com>
2021-02-16 15:35:18 +0000

    Double quotes to single quotes.

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,15 +6,15 @@ const input = require('readline-sync');
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space?"
-let correctAnswer = "Sally Ride";
+let question = 'Who was the first American woman in space? '
+let correctAnswer = 'Sally Ride';
 let candidateAnswer = "";
 let questions = [ '1) Who was the first American woman in space? ', '2) True or False: 5000 meters = 5 kilometers ', '3) (5 + 3)/2 * 10 = ? ', '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', '5) What is the minimum crew size for the ISS? ' ];
-let correctAnswers = ["Sally Ride",
-"true",
-"40",
-"Trajectory",
-"3"];
+let correctAnswers = ['Sally Ride',
+'true',
+'40',
+'Trajectory',
+'3'];
 let candidateAnswers = [];
 
 
